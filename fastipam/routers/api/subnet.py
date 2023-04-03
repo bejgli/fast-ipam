c0096e3547f93c57af2b4695f129631029f6b613
bejgli <laszlotorhosi01@gmail.com>
2023-04-03 21:40:37 +0200

    Create subnet validation

--- fastipam/routers/api/subnet.py
+++ fastipam/routers/api/subnet.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException, Response, status
+from fastapi.exception_handlers import request_validation_exception_handler
 from sqlalchemy.orm import Session
+from ipaddress import ip_network, IPv4Address, IPv6Address
 
 from fastipam import crud, schemas
 from fastipam.database import SessionLocal
@@ -45,7 +47,34 @@ def get_subnet_by_id(id: int, db: Session = Depends(get_db)):
 def create_subnet(
     subnet: schemas.SubnetCreate,
     db: Session = Depends(get_db)):
+    """Create a new subnet. Default mask is /32.
+
+    Args:
+        subnet (schemas.SubnetCreate): _description_
+        db (Session, optional): _description_. Defaults to Depends(get_db).
+
+    Raises:
+        HTTPException: _description_
+
+    Returns:
+        _type_: _description_
+    """
+
+    if crud.get_subnet_by_name(db=db, subnet_name=subnet.name):
+        raise HTTPException(
+            status_code=400, 
+            detail="Name already used"
+        )
 
     #TODO
 
     return crud.create_subnet(db=db, subnet=subnet)
+
+
+#@router.get("/addresses", status_code=201)
+#def test_subnet(subnet: schemas.SubnetCreate):
+#    if 
+#    address_range = ip_network(subnet.ip_v4).hosts()
+#    print(address_range)
+#
+#    return address_range
--- fastipam/schemas/subnet.py
+++ fastipam/schemas/subnet.py
@@ -1,19 +1,55 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator, root_validator
+from ipaddress import IPv4Network, IPv6Network, IPv4Address, IPv6Address, ip_network
+
 from .address import Address
 
 class SubnetBase(BaseModel):
-    ip_v4: int
-    ip_v6: int | None = None
+    ip_v4: str | None
+    ip_v6: str | None
     mask: str
     name: str
     description: str | None = None
+
+    @root_validator(pre=True)
+    def check_missing(cls, values):
+        if not values.get("ip_v4") and not values.get("ip_v6"):
+            raise ValueError("Either IPv4 or IPv6 must be provided")
+        
+        return values
+
+   # @validator("ip_v4", "ip_v6")
+   # def check_ip(cls, v):
+   #     if v:
+   #         ip_network(v)
+   #     return v
+
+    @validator("ip_v4")
+    def check_ipv4(cls, v):
+        if v:
+            IPv4Network(v)
+        return v
+
+    @validator("ip_v6")
+    def check_ipv6(cls, v):
+        if v:
+            IPv6Network(v)
+        return v
+
+
+#    @validator("ip_v4")
+#    def convert(cls, v):
+#        if v:
+#            return str(v)
+
 
 class SubnetCreate(SubnetBase):
     pass
 
 class Subnet(SubnetBase):
     id: int
+
     addresses: list[Address] = []
+    # address_range: list[IPv4Address | IPv6Address] = []
 
     class Config:
         orm_mode = True
