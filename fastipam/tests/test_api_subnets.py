4513b0a51103b8d65e1a328a86a42f873a336589
bejgli <laszlotorhosi01@gmail.com>
2023-04-23 22:34:38 +0200

    Create tests for host creation and deletion

--- fastipam/tests/test_api_subnets.py
+++ fastipam/tests/test_api_subnets.py
@@ -15,44 +15,57 @@ def test_create_ipv4_subnet(
     json = {
         "ip_v4": "192.168.0.0/24",
         "mask": "mask",
-        "name": "subnet1",
+        "name": "api_test_subnet",
         "description": "string",
     }
 ):
     response = client.post("/subnets/", json=json)
+
     assert response.status_code == 201
-#    assert response.json() == {
-#        "ip_v4": "192.168.0.0/24",
-#        "ip_v6": None,
-#        "mask": "string",
-#        "name": "subnet1",
-#        "description": "string",
-#        "id": 1,
-#        "addresses": []
-#    }
 
 
 def test_create_bad_name_subnet(
     json = {
-        "ip_v4": "192.168.0.1/24",
+        "ip_v4": "192.168.1.0/24",
         "mask": "mask",
-        "name": "subnet1",
+        "name": "api_test_subnet",
         "description": "string",
     }
 ):
     response = client.post("/subnets/", json=json)
-    assert response.status_code == 422
+
+    assert response.status_code == 400
 
 
 def test_read_subnet_by_id():
     id = 1
     response = client.get(f"/subnets/{id}")
+
     assert response.status_code == 200
+
+def test_create_host(
+    json = {
+        "name": "api_test_subnet",
+        "description": "string",
+        "subnet_id": 1,
+    }
+):
+    response = client.post("/addresses/", json=json)
+
+    assert response.status_code == 201
+
+
+def test_delete_host():
+    id = 1
+    response = client.delete(f"/addresses/{id}")
+
+    assert response.status_code == 204
 
 
 def test_read_subnet_by_bad_id():
-    id = 2
+    id = 99
     response = client.get(f"/subnets/{id}")
+
     assert response.status_code == 404
 
 
